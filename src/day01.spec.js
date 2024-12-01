f0b91b7cf106b772553352b27ad3b37a790557b8
Jeroen Heijmans <github@jeroenheijmans.nl>
2024-12-01 06:40:36 +0100

    Day 1 cleanup

--- src/day01.spec.js
+++ src/day01.spec.js
@@ -1,41 +1,37 @@
 import { describe, it, expect } from "bun:test";
 
+const add = (a, b) => a + b;
+
 function part1(data) {
-  let result = 0;
-  const as = data.map(x => x.first).toSorted((a,b) => a - b);
-  const bs = data.map(x => x.second).toSorted((a,b) => a - b);
-  as.forEach((a, idx) => result += Math.abs(bs[idx] - a))
-  return result
+  const as = data.map((x) => x[0]).toSorted((a, b) => a - b);
+  const bs = data.map((x) => x[1]).toSorted((a, b) => a - b);
+  return as.map((a, idx) => Math.abs(bs[idx] - a)).reduce(add, 0);
 }
 
 function part2(data) {
-  const bs = data.map(x => x.second)
+  const bs = data.map((x) => x[1]);
   return data
-    .map(x => x.first)
-    .map(a => a * bs.filter(b => b === a).length)
-    .reduce((a,b) => a + b, 0)
-} 
+    .map((x) => x[0])
+    .map((a) => a * bs.filter((b) => b === a).length)
+    .reduce(add, 0);
+}
 
 describe("day01", async () => {
   const input = await Bun.file("src/day01.txt").text();
   const data = input
     .split(/\r?\n/gi)
     .filter((x) => !!x)
-    .map(x => x.split(/\s+/ig))
-    .map(x => ({
-      first: parseInt(x[0]),
-      second: parseInt(x[1]),
-    }))
+    .map((x) => x.split(/\s+/gi).map((y) => parseInt(y)));
 
   it("should solve part 1", () => {
     const result = part1(data);
-    console.log("Day 01, part 1:", result)
+    console.log("Day 01, part 1:", result);
     expect(result).toBe(1879048);
   });
 
   it("should solve part 2", () => {
     const result = part2(data);
-    console.log("Day 01, part 2:", result)
+    console.log("Day 01, part 2:", result);
     expect(result).toBe(21024792);
   });
 });
