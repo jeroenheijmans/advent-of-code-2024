adf8bf7379bcdfed927f555d118a3a6d16f1fb9c
Jeroen Heijmans <github@jeroenheijmans.nl>
2024-12-01 22:48:57 +0100

    aoc.infi.nl prep part 2

--- src/aoc.infi.nl.spec.js
+++ src/aoc.infi.nl.spec.js
@@ -2,41 +2,39 @@ import { describe, it, expect } from "bun:test";
 
 const add = (a, b) => a + b;
 
-function part1(data) {
-  function valueFor(coords) {
-    let pointer = 0;
-    let stack = [];
+function valueFor(data, coords) {
+  let pointer = 0;
+  let stack = [];
 
-    while (true) {
-      const [instruction, subject] = data[pointer];
-      switch (instruction) {
-        case "push":
-          const value = /\d+/.test(subject)
-            ? parseInt(subject)
-            : coords[subject];
-          stack.push(value);
-          break;
-        case "add":
-          stack.push(stack.pop() + stack.pop());
-          break;
-        case "jmpos":
-          if (stack.pop() >= 0) pointer += parseInt(subject);
-          break;
-        case "ret":
-          return stack.pop();
-        default:
-          throw `Unknown instruction: ${instruction}`;
-      }
-      pointer++;
+  while (true) {
+    const [instruction, subject] = data[pointer];
+    switch (instruction) {
+      case "push":
+        const value = /\d+/.test(subject) ? parseInt(subject) : coords[subject];
+        stack.push(value);
+        break;
+      case "add":
+        stack.push(stack.pop() + stack.pop());
+        break;
+      case "jmpos":
+        if (stack.pop() >= 0) pointer += parseInt(subject);
+        break;
+      case "ret":
+        return stack.pop();
+      default:
+        throw `Unknown instruction: ${instruction}`;
     }
+    pointer++;
   }
+}
 
+function part1(data) {
   let result = 0;
 
   for (let x = 0; x < 30; x++) {
     for (let y = 0; y < 30; y++) {
       for (let z = 0; z < 30; z++) {
-        result += valueFor({ x, y, z });
+        result += valueFor(data, { x, y, z });
       }
     }
   }
@@ -71,10 +69,10 @@ describe("aoc.infi.nl", async () => {
     expect(result).toBe(5108);
   });
 
-  // it("should solve part 2", async () => {
-  //   const data = await getDataFrom("src/aoc.infi.nl.txt");
-  //   const result = part2(data);
-  //   console.log("Infi AoC Sponsor Puzzle, part 2:", result);
-  //   expect(result).toBe(0);
-  // });
+  it("should solve part 2", async () => {
+    const data = await getDataFrom("src/aoc.infi.nl.txt");
+    const result = part2(data);
+    console.log("Infi AoC Sponsor Puzzle, part 2:", result);
+    expect(result).toBe(0);
+  });
 });
