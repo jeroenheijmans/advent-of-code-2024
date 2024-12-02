49747945e2e10bb1fbdebce798677a7466aeb312
Jeroen Heijmans <github@jeroenheijmans.nl>
2024-12-02 08:36:49 +0100

    Day 02 part 1

--- src/day02.spec.js
+++ src/day02.spec.js
@@ -1,27 +1,61 @@
 import { describe, it, expect } from "bun:test";
 
 function part1(data) {
-  return data.length;
+  function isSafe(report) {
+    // console.log(report);
+    const isIncreasing = report[1] > report[0];
+    for (let i = 0; i < report.length - 1; i++) {
+      const diff = report[i + 1] - report[i];
+      const isMovingCorrectly = isIncreasing ? diff > 0 : diff < 0;
+      const absDiff = Math.abs(diff);
+      if (isMovingCorrectly && absDiff <= 3) continue;
+      return false;
+    }
+    return true;
+  }
+  const safeReports = data.filter(r => isSafe(r));
+  // console.log(safeReports);
+  return safeReports.length;
 }
 
 function part2(data) {
   return 0;
 }
 
-describe("day02", async () => {
-  const input = await Bun.file("src/day02.txt").text();
-  const data = input
+function parseInput(input) {
+  return input
+    .trim()
     .split(/\r?\n/gi)
-    .filter((x) => !!x);
+    .filter((x) => !!x)
+    .map(x => x.split(/\s+/ig).map(x => parseInt(x)));
+}
+
+describe("day02", async () => {
+  const example = `
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+  `;
+
+  const input = await Bun.file("src/day02.txt").text();
+
+  it("should solve part 1 (example)", () => {
+    const result = part1(parseInput(example));
+    console.log("Day 02, part 1:", result);
+    expect(result).toBe(2);
+  });
 
   it("should solve part 1", () => {
-    const result = part1(data);
+    const result = part1(parseInput(input));
     console.log("Day 02, part 1:", result);
     expect(result).toBe(0);
   });
 
   it("should solve part 2", () => {
-    const result = part2(data);
+    const result = part2(parseInput(input));
     console.log("Day 02, part 2:", result);
     expect(result).toBe(0);
   });
