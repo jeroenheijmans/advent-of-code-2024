bf645533372d8b2981d422e04d6ab901c8dca377
Jeroen Heijmans <github@jeroenheijmans.nl>
2024-12-02 08:45:27 +0100

    Day 02 part 2 (incorrect, 657)

--- src/day02.spec.js
+++ src/day02.spec.js
@@ -21,20 +21,24 @@ function part1(data) {
 function part2(data) {
   function isSafe(report) {
     let errors = 0;
+    let errorWasJustNow = false;
     // console.log(report);
     const isIncreasing = report[1] > report[0];
     for (let i = 0; i < report.length - 1; i++) {
-      const diff = report[i + 1] - report[errors > 0 ? i -1 : i];
+      const offset = errorWasJustNow && errors > 0 ? 1 : 0;
+      const diff = report[i + 1] - report[i - offset];
+      errorWasJustNow = false;
       const isMovingCorrectly = isIncreasing ? diff > 0 : diff < 0;
       const absDiff = Math.abs(diff);
       if (isMovingCorrectly && absDiff <= 3) continue;
       errors++;
+      errorWasJustNow = true;
       if (errors > 1) return false;
     }
     return true;
   }
-  const safeReports = data.filter(r => isSafe(r));
-  // console.log(safeReports);
+  const safeReports = data.filter(r => !isSafe(r));
+  safeReports.forEach(x => console.log(x));
   return safeReports.length;
 }
 
@@ -58,17 +62,23 @@ describe("day02", async () => {
 
   const input = await Bun.file("src/day02.txt").text();
 
-  it("should solve part 1 (example)", () => {
-    const result = part1(parseInput(example));
-    console.log("Day 02, part 1:", result);
-    expect(result).toBe(2);
-  });
+  // it("should solve part 1 (example)", () => {
+  //   const result = part1(parseInput(example));
+  //   console.log("Day 02, part 1 (example):", result);
+  //   expect(result).toBe(2);
+  // });
 
-  it("should solve part 1", () => {
-    const result = part1(parseInput(input));
-    console.log("Day 02, part 1:", result);
-    expect(result).toBe(0);
-  });
+  // it("should solve part 1", () => {
+  //   const result = part1(parseInput(input));
+  //   console.log("Day 02, part 1:", result);
+  //   expect(result).toBe(0);
+  // });
+
+  // it("should solve part 2 (example)", () => {
+  //   const result = part2(parseInput(example));
+  //   console.log("Day 02, part 2 (example):", result);
+  //   expect(result).toBe(4);
+  // });
 
   it("should solve part 2", () => {
     const result = part2(parseInput(input));
