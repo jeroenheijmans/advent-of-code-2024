9b211b6244ddce7c77d880486383937a1c790ec4
Jeroen Heijmans <github@jeroenheijmans.nl>
2024-12-03 08:21:18 +0100

    Day 04 prep

---
+++ src/day04.spec.js
@@ -0,0 +1,46 @@
+import { describe, it, expect } from "bun:test";
+
+function part1(data) {
+  return data.length;
+}
+
+function part2(data) {
+  return data.length;
+}
+
+function parseInput(input) {
+  return input
+    .trim()
+    .split(/\r?\n/g)
+    .filter((x) => !!x);
+}
+
+describe("day04", async () => {
+  const example = ``;
+
+  const input = await Bun.file("src/day04.txt").text();
+
+  it("should solve part 1 (example)", () => {
+    const result = part1(parseInput(example));
+    console.log("Day 04, part 1 (example):", result);
+    expect(result).toBe(0);
+  });
+
+  // it("should solve part 1", () => {
+  //   const result = part1(parseInput(input));
+  //   console.log("Day 04, part 1:", result);
+  //   expect(result).toBe(0);
+  // });
+
+  // it("should solve part 2 (example)", () => {
+  //   const result = part2(parseInput(example));
+  //   console.log("Day 04, part 2 (example):", result);
+  //   expect(result).toBe(0);
+  // });
+
+  // it("should solve part 2", () => {
+  //   const result = part2(parseInput(input));
+  //   console.log("Day 04, part 2:", result);
+  //   expect(result).toBe(0);
+  // });
+});
