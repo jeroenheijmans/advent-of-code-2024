a2a0e1d9161d8990cbfdd023bda20e1792ddd060
Jeroen Heijmans <github@jeroenheijmans.nl>
2024-12-03 08:06:00 +0100

    Day 03 intermediate commit

--- src/day03.spec.js
+++ src/day03.spec.js
@@ -3,13 +3,11 @@ import { describe, it, expect } from "bun:test";
 const add = (a, b) => a + b;
 
 function part1(data) {
-  return data.map(line => {
-    const matches = line.match(/mul\(\d+,\d+\)/g);
-    return matches ? matches.map(match => {
-      const [a, b] = match.match(/\d+/g).map(Number);
-      return a * b;
-    }).reduce(add, 0) : 0;
-  }).reduce(add, 0);
+  const matches = data.match(/mul\(\d+,\d+\)/g);
+  return matches ? matches.map(match => {
+    const [a, b] = match.match(/\d+/g).map(Number);
+    return a * b;
+  }).reduce(add, 0) : 0;
 }
 
 function part2(data) {
@@ -17,21 +15,21 @@ function part2(data) {
 }
 
 function parseInput(input) {
-  return input
-    .trim()
-    .split(/\r?\n/)
-    .filter((x) => !!x);
+  return input.trim();
 }
 
 describe("day03", async () => {
-  const example = `
+  const example1 = `
 xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+  `;
+  const example2 = `
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
   `;
 
   const input = await Bun.file("src/day03.txt").text();
 
   it("should solve part 1 (example)", () => {
-    const result = part1(parseInput(example));
+    const result = part1(parseInput(example1));
     console.log("Day 03, part 1 (example):", result);
     expect(result).toBe(161);
   });
@@ -42,11 +40,11 @@ xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
     expect(result).toBe(183669043);
   });
 
-  // it("should solve part 2 (example)", () => {
-  //   const result = part2(parseInput(example));
-  //   console.log("Day 03, part 2 (example):", result);
-  //   expect(result).toBe(0);
-  // });
+  it("should solve part 2 (example)", () => {
+    const result = part2(parseInput(example2));
+    console.log("Day 03, part 2 (example):", result);
+    expect(result).toBe(48);
+  });
 
   // it("should solve part 2", () => {
   //   const result = part2(parseInput(input));
