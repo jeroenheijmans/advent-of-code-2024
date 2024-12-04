e958d0aa3578625a187ae0f5ee222e902f0e901e
Jeroen Heijmans <github@jeroenheijmans.nl>
2024-12-04 06:58:49 +0100

    Day 05 prep

---
+++ src/day05.spec.js
@@ -0,0 +1,50 @@
+import { describe, it, expect } from "bun:test";
+
+const day = "05";
+
+function part1(data) {
+  return data.length;
+}
+
+function part2(data) {
+  return data.length;
+}
+
+function parseInput(input) {
+  return input
+    .trim()
+    .split(/\r?\n/g)
+    .filter((x) => !!x)
+    .map((x) => x.split(""));
+}
+
+describe(`day${day}`, async () => {
+  const example = `
+  `;
+
+  const input = await Bun.file(`src/day${day}.txt`).text();
+
+  it("should solve part 1 (example)", () => {
+    const result = part1(parseInput(example));
+    console.log(`Day ${day}, part 1 (example):`, result);
+    expect(result).toBe(0);
+  });
+
+  it("should solve part 1", () => {
+    const result = part1(parseInput(input));
+    console.log(`Day ${day}, part 1:`, result);
+    expect(result).toBe(0);
+  });
+
+  it("should solve part 2 (example)", () => {
+    const result = part2(parseInput(example));
+    console.log(`Day ${day}, part 2 (example):`, result);
+    expect(result).toBe(0);
+  });
+
+  it("should solve part 2", () => {
+    const result = part2(parseInput(input));
+    console.log(`Day ${day}, part 2:`, result);
+    expect(result).toBe(0);
+  });
+});
