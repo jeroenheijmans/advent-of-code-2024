0c765577b87f30d83c9717af0a6d4d2b2522db90
Jeroen Heijmans <github@jeroenheijmans.nl>
2024-12-06 14:56:04 +0100

    Day 06 part 1

--- src/day06.spec.js
+++ src/day06.spec.js
@@ -3,7 +3,31 @@ import { describe, it, expect } from "bun:test";
 const day = "06";
 
 function part1(data) {
-  return data.length;
+  const maxx = Math.max(...data.map((p) => p.x));
+  const maxy = Math.max(...data.map((p) => p.y));
+  const obstacles = data.filter((p) => p.char === "#");
+  const map = new Map(obstacles.map((p) => [p.key, p]));
+
+  let { x, y } = data.find((p) => p.char === "^");
+  let dir = { dx: 0, dy: -1 };
+  const visited = new Set();
+
+  while (x >= 0 && y >= 0 && x <= maxx && y <= maxy) {
+    const key = `${x};${y}`;
+    const next = `${x + dir.dx};${y + dir.dy}`;
+    visited.add(key);
+
+    if (map.get(next)) {
+      if (dir.dy === -1) dir = { dx: +1, dy: 0 };
+      else if (dir.dy === +1) dir = { dx: -1, dy: 0 };
+      else if (dir.dx === -1) dir = { dx: 0, dy: -1 };
+      else if (dir.dx === +1) dir = { dx: 0, dy: +1 };
+    } else {
+      x += dir.dx;
+      y += dir.dy;
+    }
+  }
+  return visited.size;
 }
 
 function part2(data) {
@@ -14,11 +38,31 @@ function parseInput(input) {
   return input
     .trim()
     .split(/\r?\n/g)
-    .filter((x) => !!x);
+    .filter((x) => !!x)
+    .map((line) => line.split(""))
+    .map((line, y) =>
+      line.map((char, x) => ({
+        key: `${x};${y}`,
+        x,
+        y,
+        char,
+      }))
+    )
+    .flat();
 }
 
 describe(`day${day}`, async () => {
   const example = `
+....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...
   `;
 
   const input = await Bun.file(`src/day${day}.txt`).text();
@@ -26,7 +70,7 @@ describe(`day${day}`, async () => {
   it("should solve part 1 (example)", () => {
     const result = part1(parseInput(example));
     console.log(`Day ${day}, part 1 (example):`, result);
-    expect(result).toBe(0);
+    expect(result).toBe(41);
   });
 
   it("should solve part 1", () => {
@@ -35,15 +79,15 @@ describe(`day${day}`, async () => {
     expect(result).toBe(0);
   });
 
-  it("should solve part 2 (example)", () => {
-    const result = part2(parseInput(example));
-    console.log(`Day ${day}, part 2 (example):`, result);
-    expect(result).toBe(0);
-  });
+  // it("should solve part 2 (example)", () => {
+  //   const result = part2(parseInput(example));
+  //   console.log(`Day ${day}, part 2 (example):`, result);
+  //   expect(result).toBe(0);
+  // });
 
-  it("should solve part 2", () => {
-    const result = part2(parseInput(input));
-    console.log(`Day ${day}, part 2:`, result);
-    expect(result).toBe(0);
-  });
+  // it("should solve part 2", () => {
+  //   const result = part2(parseInput(input));
+  //   console.log(`Day ${day}, part 2:`, result);
+  //   expect(result).toBe(0);
+  // });
 });
