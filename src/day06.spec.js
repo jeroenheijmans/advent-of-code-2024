a2aba5e9a107a819037bd556025aaef253b41122
Jeroen Heijmans <github@jeroenheijmans.nl>
2024-12-06 15:09:19 +0100

    Day 06 part 2 (slooowwww)

--- src/day06.spec.js
+++ src/day06.spec.js
@@ -2,7 +2,7 @@ import { describe, it, expect } from "bun:test";
 
 const day = "06";
 
-function part1(data) {
+function findRouteSize(data) {
   const maxx = Math.max(...data.map((p) => p.x));
   const maxy = Math.max(...data.map((p) => p.y));
   const obstacles = data.filter((p) => p.char === "#");
@@ -11,11 +11,16 @@ function part1(data) {
   let { x, y } = data.find((p) => p.char === "^");
   let dir = { dx: 0, dy: -1 };
   const visited = new Set();
+  const visitsWithDirection = new Set();
 
   while (x >= 0 && y >= 0 && x <= maxx && y <= maxy) {
     const key = `${x};${y}`;
     const next = `${x + dir.dx};${y + dir.dy}`;
     visited.add(key);
+
+    const masterKey = `${key};${dir.dx};${dir.dy}`;
+    if (visitsWithDirection.has(masterKey)) throw "Loop found!";
+    visitsWithDirection.add(masterKey);
 
     if (map.get(next)) {
       if (dir.dy === -1) dir = { dx: +1, dy: 0 };
@@ -30,8 +35,36 @@ function part1(data) {
   return visited.size;
 }
 
+function part1(data) {
+  return findRouteSize(data);
+}
+
 function part2(data) {
-  return data.length;
+  const maxx = Math.max(...data.map((p) => p.x));
+  const maxy = Math.max(...data.map((p) => p.y));
+  const obstacles = data.filter((p) => p.char === "#");
+  const map = new Map(obstacles.map((p) => [p.key, p]));
+
+  let { x, y } = data.find((p) => p.char === "^");
+  let result = 0;
+
+  for (let y2 = 0; y2 <= maxy; y2++) {
+    for (let x2 = 0; x2 <= maxx; x2++) {
+      const newKey = `${x2};${y2}`;
+
+      if (x2 === x && y2 === y) continue;
+      if (map.get(newKey)) continue;
+
+      try {
+        const newData = [...data, { x: x2, y: y2, key: newKey, char: "#"}];
+        const _size = findRouteSize(newData);
+      } catch (error) {
+        if (error === "Loop found!") result++;
+        else throw error;
+      }
+    }
+  }
+  return result;
 }
 
 function parseInput(input) {
@@ -76,18 +109,18 @@ describe(`day${day}`, async () => {
   it("should solve part 1", () => {
     const result = part1(parseInput(input));
     console.log(`Day ${day}, part 1:`, result);
-    expect(result).toBe(0);
+    expect(result).toBe(5531);
   });
 
-  // it("should solve part 2 (example)", () => {
-  //   const result = part2(parseInput(example));
-  //   console.log(`Day ${day}, part 2 (example):`, result);
-  //   expect(result).toBe(0);
-  // });
+  it("should solve part 2 (example)", () => {
+    const result = part2(parseInput(example));
+    console.log(`Day ${day}, part 2 (example):`, result);
+    expect(result).toBe(6);
+  });
 
-  // it("should solve part 2", () => {
-  //   const result = part2(parseInput(input));
-  //   console.log(`Day ${day}, part 2:`, result);
-  //   expect(result).toBe(0);
-  // });
+  it("should solve part 2", () => {
+    const result = part2(parseInput(input));
+    console.log(`Day ${day}, part 2:`, result);
+    expect(result).toBe(2165);
+  });
 });
