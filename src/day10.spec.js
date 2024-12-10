9875a4f607dbbb9aa276cbe593e085e84f80c90f
Jeroen Heijmans <github@jeroenheijmans.nl>
2024-12-10 06:29:07 +0100

    Day 10 re-enable part 1

--- src/day10.spec.js
+++ src/day10.spec.js
@@ -99,17 +99,17 @@ describe(`day${day}`, async () => {
 
   const input = await Bun.file(`src/day${day}.txt`).text();
 
-  // it("should solve part 1 (example)", () => {
-  //   const result = part1(parseInput(example));
-  //   console.log(`Day ${day}, part 1 (example):`, result);
-  //   expect(result).toBe(36);
-  // });
+  it("should solve part 1 (example)", () => {
+    const result = part1(parseInput(example));
+    console.log(`Day ${day}, part 1 (example):`, result);
+    expect(result).toBe(36);
+  });
 
-  // it("should solve part 1", () => {
-  //   const result = part1(parseInput(input));
-  //   console.log(`Day ${day}, part 1:`, result);
-  //   expect(result).toBe(688);
-  // });
+  it("should solve part 1", () => {
+    const result = part1(parseInput(input));
+    console.log(`Day ${day}, part 1:`, result);
+    expect(result).toBe(688);
+  });
 
   it("should solve part 2 (example)", () => {
     const result = part2(parseInput(example));
