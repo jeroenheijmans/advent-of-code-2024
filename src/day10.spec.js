f7601132ff47ed4c28440bdd534795375a6dba50
Jeroen Heijmans <github@jeroenheijmans.nl>
2024-12-10 06:23:24 +0100

    Day 10 part 1 (with recursion... uh oh!)

--- src/day10.spec.js
+++ src/day10.spec.js
@@ -3,7 +3,37 @@ import { describe, it, expect } from "bun:test";
 const day = "10";
 
 function part1(data) {
-  return data.length;
+  const maxx = Math.max(...data.map((p) => p.x));
+  const maxy = Math.max(...data.map((p) => p.y));
+
+  const map = data.reduce((result, next) => {
+    result[next.key] = next;
+    return result;
+  }, {});
+
+  const zeros = data.filter((p) => p.value === 0);
+
+  function neighbors({ x, y }) {
+    const result = [];
+    if (x > 0 && y >= 0 && y <= maxy) result.push(map[`${x - 1};${y}`]);
+    if (y > 0 && x >= 0 && x <= maxx) result.push(map[`${x};${y - 1}`]);
+    if (x < maxx && y >= 0 && y <= maxy) result.push(map[`${x + 1};${y}`]);
+    if (y < maxy && x >= 0 && x <= maxy) result.push(map[`${x};${y + 1}`]);
+    return result;
+  }
+
+  function recursivelyFindEnds(current) {
+    if (current.value === 9) return [current.key];
+    const target = current.value + 1;
+    return neighbors(current)
+      .filter((neighbor) => neighbor.value === target)
+      .map((neighbor) => recursivelyFindEnds(neighbor))
+      .flat();
+  }
+
+  return zeros
+    .map((z) => new Set(recursivelyFindEnds(z)))
+    .reduce((p, c) => p + c.size, 0);
 }
 
 function part2(data) {
@@ -11,25 +41,45 @@ function part2(data) {
 }
 
 function parseInput(input) {
-  return input.trim().split("");
+  return input
+    .trim()
+    .split(/\r?\n/g)
+    .map((line, y) =>
+      line.split("").map((value, x) => ({
+        x,
+        y,
+        key: `${x};${y}`,
+        value: parseInt(value),
+      }))
+    )
+    .flat();
 }
 
 describe(`day${day}`, async () => {
-  const example = ``;
+  const example = `
+89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732
+  `;
 
   const input = await Bun.file(`src/day${day}.txt`).text();
 
   it("should solve part 1 (example)", () => {
     const result = part1(parseInput(example));
     console.log(`Day ${day}, part 1 (example):`, result);
-    expect(result).toBe(0);
+    expect(result).toBe(36);
   });
 
-  // it("should solve part 1", () => {
-  //   const result = part1(parseInput(input));
-  //   console.log(`Day ${day}, part 1:`, result);
-  //   expect(result).toBe(0);
-  // });
+  it("should solve part 1", () => {
+    const result = part1(parseInput(input));
+    console.log(`Day ${day}, part 1:`, result);
+    expect(result).toBe(688);
+  });
 
   // it("should solve part 2 (example)", () => {
   //   const result = part2(parseInput(example));
