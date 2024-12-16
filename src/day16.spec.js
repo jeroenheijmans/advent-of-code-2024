6a9b9acdeeba3891edaacc4674bb7f36004a14c2
Jeroen Heijmans <github@jeroenheijmans.nl>
2024-12-17 00:29:26 +0100

    Day 16 part 1

--- src/day16.spec.js
+++ src/day16.spec.js
@@ -4,10 +4,10 @@ const day = "16";
 
 const getKey = (x, y) => `${x};${y}`;
 const dirs = [
-  { dx: -1, dy: +0 },
-  { dx: +0, dy: +1 },
-  { dx: +1, dy: +0 },
-  { dx: +0, dy: -1 },
+  { dx: -1, dy: +0, key: "west" },
+  { dx: +0, dy: +1, key: "south" },
+  { dx: +1, dy: +0, key: "east" },
+  { dx: +0, dy: -1, key: "north" },
 ];
 
 function part1(data) {
@@ -72,14 +72,65 @@ function part1(data) {
   } while ((next = findFoldable()));
 
   // https://mermaid.live/
-  console.log("flowchart TD");
-  Object.values(maze).forEach((p) => {
-    p.links.forEach((l) => {
-      console.log(`  ${p.label} --> |${l.weight}| ${l.target.label}`);
-    });
-  });
+  // console.log("flowchart TD");
+  // Object.values(maze).forEach((p) => {
+  //   p.links.forEach((l) => {
+  //     console.log(`  ${p.label} --> |${l.weight}| ${l.target.label}`);
+  //   });
+  // });
 
-  return data.length;
+  function findCheapestPath() {
+    let dir = dirs.find(d => d.key === "east");
+    let edges = [{ target: start, dir, cost: 0 }];
+    const seen = new Set();
+    while (edges.length > 0) {
+      let newEdges = [];
+      const cheapestCost = Math.min(...edges.map(e => e.cost));
+
+      // console.log("Considering cheapest cost", cheapestCost);
+      // console.log(edges);
+
+      for (const edge of edges) {
+        if (edge.cost > cheapestCost) {
+          newEdges.push(edge);
+          continue;
+        }
+
+        if (edge.target === finish) return edge.cost;
+
+        const key = edge.target.key;
+        if (seen.has(key)) continue;
+
+        seen.add(key);
+
+        // Continue in current direction
+        const simpleNextStep = edge.target.links.find(l => l.dir === edge.dir);
+        if (simpleNextStep) {
+          newEdges.push({
+            target: simpleNextStep.target,
+            cost: edge.cost + simpleNextStep.weight,
+            dir: edge.dir
+          });
+        }
+
+        // Steps that require 90deg turn
+        edge.target.links.filter(l => l !== simpleNextStep && l.target !== edge.target)
+          .forEach(step => {
+            // console.log("Turning at", key, "towards", step.target.key)
+            newEdges.push({
+              target: step.target,
+              cost: edge.cost + step.weight + 1000,
+              dir: step.dir,
+            });
+          });
+      }
+      edges = newEdges;
+    }
+
+    throw "No solution found!";
+  }
+
+  return findCheapestPath();
 }
 
 function part2(data) {
@@ -161,23 +212,23 @@ describe(`day${day}`, async () => {
     expect(result).toBe(2005);
   });
 
-  // it("should solve part 1 (example 1)", () => {
-  //   const result = part1(parseInput(example1));
-  //   console.log(`Day ${day}, part 1 (example 1):`, result);
-  //   expect(result).toBe(7036);
-  // });
+  it("should solve part 1 (example 1)", () => {
+    const result = part1(parseInput(example1));
+    console.log(`Day ${day}, part 1 (example 1):`, result);
+    expect(result).toBe(7036);
+  });
 
-  // it("should solve part 1 (example 2)", () => {
-  //   const result = part1(parseInput(example2));
-  //   console.log(`Day ${day}, part 1 (example 2):`, result);
-  //   expect(result).toBe(11048);
-  // });
+  it("should solve part 1 (example 2)", () => {
+    const result = part1(parseInput(example2));
+    console.log(`Day ${day}, part 1 (example 2):`, result);
+    expect(result).toBe(11048);
+  });
 
-  // it("should solve part 1", () => {
-  //   const result = part1(parseInput(input));
-  //   console.log(`Day ${day}, part 1:`, result);
-  //   expect(result).toBe(0);
-  // });
+  it("should solve part 1", () => {
+    const result = part1(parseInput(input));
+    console.log(`Day ${day}, part 1:`, result);
+    expect(result).toBe(95476);
+  });
 
   // it("should solve part 2 (example)", () => {
   //   const result = part2(parseInput(example));
