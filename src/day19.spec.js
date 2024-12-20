ab2c58302571948304b02b6a86186a6a898eeb33
Jeroen Heijmans <github@jeroenheijmans.nl>
2024-12-20 16:05:19 +0100

    Day 19 part 1

--- src/day19.spec.js
+++ src/day19.spec.js
@@ -2,8 +2,29 @@ import { describe, it, expect } from "bun:test";
 
 const day = "19";
 
-function part1(data) {
-  return data.length;
+function part1({patterns, designs}) {
+
+  const possibles = new Set([""]);
+  const impossibles = new Set();
+
+  function isPossible(design) {
+    if (possibles.has(design)) return true;
+    if (impossibles.has(design)) return false;
+
+    for (const pattern of patterns) {
+      if (design.startsWith(pattern)) {
+        const rest = design.substring(pattern.length);
+        if (isPossible(rest)) {
+          possibles.add(design);
+          return true;
+        }
+      }
+    }
+    impossibles.add(design);
+    return false;
+  }
+
+  return designs.filter(d => isPossible(d)).length;
 }
 
 function part2(data) {
@@ -11,39 +32,53 @@ function part2(data) {
 }
 
 function parseInput(input) {
-  return input
+  const [patterns, designs] = input
     .trim()
-    .split(/\r?\n/g)
-    .filter((x) => !!x);
+    .split(/\r?\n\r?\n/g);
+  
+  return {
+    patterns: patterns.split(", "),
+    designs: designs.split(/\r?\n/g),
+  };
 }
 
 describe(`day${day}`, async () => {
   const example1 = `
+r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb
   `;
 
   const input = await Bun.file(`src/day${day}.txt`).text();
 
-  it("should solve part 1 (example)", () => {
+  it("should solve part 1 (example 1)", () => {
     const result = part1(parseInput(example1));
-    console.log(`Day ${day}, part 1 (example):`, result);
-    expect(result).toBe(0);
+    console.log(`Day ${day}, part 1 (example 1):`, result);
+    expect(result).toBe(6);
   });
 
   it("should solve part 1", () => {
     const result = part1(parseInput(input));
     console.log(`Day ${day}, part 1:`, result);
-    expect(result).toBe(0);
+    expect(result).toBe(302);
   });
 
-  it("should solve part 2 (example)", () => {
-    const result = part2(parseInput(example1));
-    console.log(`Day ${day}, part 2 (example):`, result);
-    expect(result).toBe(0);
-  });
+  // it("should solve part 2 (example 1)", () => {
+  //   const result = part2(parseInput(example1));
+  //   console.log(`Day ${day}, part 2 (example 1):`, result);
+  //   expect(result).toBe(0);
+  // });
 
-  it("should solve part 2", () => {
-    const result = part2(parseInput(input));
-    console.log(`Day ${day}, part 2:`, result);
-    expect(result).toBe(0);
-  });
+  // it("should solve part 2", () => {
+  //   const result = part2(parseInput(input));
+  //   console.log(`Day ${day}, part 2:`, result);
+  //   expect(result).toBe(0);
+  // });
 });
