304db1932efc36bac9fbc1370a3cc02ba696f355
Jeroen Heijmans <github@jeroenheijmans.nl>
2024-12-24 23:04:49 +0100

    Day 25 prep

---
+++ src/day25.spec.js
@@ -0,0 +1,43 @@
+import { describe, it, expect } from "bun:test";
+
+const day = "25";
+
+function part1(data) {
+  return data.length;
+}
+
+function part2(_) {
+  return "⭐️";
+}
+
+function parseInput(input) {
+  return input
+    .trim()
+    .split(/\r?\n/g)
+    .filter((x) => !!x);
+}
+
+describe(`day${day}`, async () => {
+  const example1 = `
+  `;
+
+  const input = await Bun.file(`src/day${day}.txt`).text();
+
+  it("should solve part 1 (example 1)", () => {
+    const result = part1(parseInput(example1));
+    console.log(`Day ${day}, part 1 (example 1):`, result);
+    expect(result).toBe(0);
+  });
+
+  // it("should solve part 1", () => {
+  //   const result = part1(parseInput(input));
+  //   console.log(`Day ${day}, part 1:`, result);
+  //   expect(result).toBe(0);
+  // });
+
+  // it("should solve part 2", () => {
+  //   const result = part2(parseInput(input));
+  //   console.log(`Day ${day}, part 2:`, result);
+  //   expect(result).toBe("⭐️");
+  // });
+});
